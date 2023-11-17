74810cb6dce6a84e95c5a16dfc371aa97192c40f
Rayne <kdlittle2000@outlook.com>
2023-11-17 11:02:47 -0500

    updated controller

--- src/levelup/controller.py
+++ src/levelup/controller.py
@@ -23,8 +23,8 @@ class GameController:
         self.status = GameStatus()
 
     def start_game(self):
-        # TODO: implement method here and remove the print statement below
-        print("start_game method not yet implemented")            
+        if not hasattr(self, 'character'):
+            return           
 
         # Status code is written for you
         self.status.current_position = (self.character.current_position.x, self.character.current_position.y)
@@ -36,8 +36,8 @@ class GameController:
         self.status.character_name = self.character.name
 
     def move(self, direction: Direction) -> None:
-        # TODO: implement method here and remove the print statement below
-        print("move method not yet implemented")
+        if not hasattr(self, 'character') or not hasattr(self, 'map'):
+            return
 
         # Status code is written for you
         self.status.current_position = (self.character.current_position.x, self.character.current_position.y)
